3ed53d8606659649624228a00e9e8a7b5e3df5f3
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2022-03-04 23:44:23 -0300

    Solve responsive for gods gallery

--- src/app/components/gods/gods.component.ts
+++ src/app/components/gods/gods.component.ts
@@ -7,17 +7,17 @@ import {
 } from '@angular/core';
 import { SwiperComponent } from 'swiper/angular';
 import SwiperCore, {
-  SwiperOptions,Pagination, Swiper, Zoom, Navigation, FreeMode, Scrollbar } from 'swiper';
+  SwiperOptions,Pagination, Swiper, Zoom, Navigation, FreeMode, Scrollbar, EffectCoverflow  } from 'swiper';
 
-SwiperCore.use([Pagination, Zoom, Navigation, FreeMode, Scrollbar ]);
+SwiperCore.use([Pagination, Zoom, Navigation, FreeMode, Scrollbar, EffectCoverflow ]);
 @Component({
   selector: 'app-gods',
   templateUrl: './gods.component.html',
   styleUrls: ['./gods.component.scss'],
 })
 export class GodsComponent implements OnInit {
-  @ViewChild('swiper') swiper: SwiperComponent | undefined;
 
+  @ViewChild('swiper') swiper: SwiperComponent | undefined;
   onResize() {}
 
   onSlideChange() {
@@ -38,44 +38,41 @@ export class GodsComponent implements OnInit {
     }
 
     const swiper = new Swiper('.gods_swiper', {
-      // Default parameters
-      spaceBetween: 100,
-      //freeMode: true,
-      zoom: {
-        maxRatio: 3,
-      },
-      pagination: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      freeMode: false,
-      loop: true,
-      centeredSlides: true,
-      scrollbar: false,
-      effect: 'slide',
-      // Responsive breakpoints
-      breakpoints: {
-        // when window width is >= 320px
-        500: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        800: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        // when window width is >= 480px
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-        },
-        // when window width is >= 640px
-        1500: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-      },
-    });
+     // Default parameters
+     spaceBetween: 100,
+     //freeMode: true,
+     zoom: {
+       maxRatio: 3,
+     },
+     pagination: true,
+     navigation: {
+       nextEl: '.god_swiper-button-next',
+       prevEl: '.god_swiper-button-prev',
+     },
+     loop: true,
+     centeredSlides: true,
+     // Responsive breakpoints
+     breakpoints: {
+       // when window width is >= 320px
+       500: {
+         slidesPerView: 1,
+         spaceBetween: 20,
+       },
+       800: {
+         slidesPerView: 2,
+         spaceBetween: 20,
+       },
+       // when window width is >= 480px
+       1024: {
+         slidesPerView: 3,
+         spaceBetween: 30,
+       },
+       // when window width is >= 640px
+       1500: {
+         slidesPerView: 4,
+         spaceBetween: 40,
+       },
+     },
+   });
   }
 }
