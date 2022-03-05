6f772aefca84c759cfb9bd4c91723457be6c9d15
Misos .- <diegosteamj3@gmail.com>
2022-03-04 22:58:13 -0300

    Social media buttons added

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faDiscord, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-banner',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
+
+  faDiscord = faDiscord;
+  faInstagram = faInstagram;
+  faTwitter = faTwitter;
 
   constructor() { }
 
